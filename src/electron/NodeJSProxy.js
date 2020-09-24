b0c0502c20c6d435202b0521e8eb0668bce2cf37
Dbl O Sevn (Doug) <doug@geauxpreaux.tech>
2020-09-24 10:57:38 -0500

    Add missing file and fix path

---
+++ src/electron/NodeJSProxy.js
@@ -0,0 +1,11 @@
+module.exports = exports = {
+    start: function() {
+        console.log('Start Node')
+    },
+    startWithScript: function() {
+        console.log('Start With Script');
+    },
+    channel: function() {
+        console.log('Channel');
+    }
+}
