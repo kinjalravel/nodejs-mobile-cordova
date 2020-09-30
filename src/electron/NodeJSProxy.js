1104f406ac984fe788910251ed1440352ce544b8
Dbl O Sevn (Doug) <doug@geauxpreaux.tech>
2020-09-30 02:00:33 -0500

    Add handlers

--- src/electron/NodeJSProxy.js
+++ src/electron/NodeJSProxy.js
@@ -1,16 +1,28 @@
 module.exports = exports = {
     sendMessageToNode: function(success, error, args) {
+        console.log('Client::startEngine::Result', result);
         Electron.ipcRenderer.send('sendMessageToNode', args);
     },
     startEngine: function(success, error, args) {
-        success();
+        console.log('Client::startEngine', args);
+        Electron.ipcRenderer.invoke('startEngine').then((result) => {
+            console.log('startEngine::Result', result);
+            success(result);
+        });
     },
     startEngineWithScript: function(success, error, args) {
-        success();
-        console.log('startEngineWithScript', arguments);
+        console.log('Client::startEngineWithScript', arguments);
+        Electron.ipcRenderer.invoke('startEngineWithScript', args).then((result) => {
+            console.log('Client::startEngineWithScript::Result', result);
+            success(result);
+        });
     },
     setAllChannelsListener: function(success, error, args) {
-        console.log('setAllChannelsListener', arguments);
+        console.log('Client::setAllChannelsListener', arguments);
+        Electron.ipcRenderer.invoke('setAllChannelsListener', args).then((result) => {
+            console.log('Client::setAllChannelsListener::Result', result);
+            success(result);
+        });
     }
 }
 cordova.commandProxy.add('NodeJS', exports);
