4e423c9d22946f076da9230e74143cb89593311c
Dbl O Sevn (Doug) <doug@geauxpreaux.tech>
2020-09-30 04:43:18 -0500

    Remove Debugging code

--- src/electron/NodeJSProxy.js
+++ src/electron/NodeJSProxy.js
@@ -1,31 +1,19 @@
 module.exports = exports = {
     sendMessageToNode: function(success, error, args) {
-        console.log('Client::sendMessageToNode', args);
         Electron.ipcRenderer.send('sendMessageToNode', args);
     },
     startEngine: function(success, error, args) {
-        console.log('Client::startEngine', args);
         Electron.ipcRenderer.once('startEngineResponse', (event, ...args) => {
-            console.log('Client::startEngine::Result', ...args);
             success(args);
         });
         Electron.ipcRenderer.send('startEngine', args);
     },
     startEngineWithScript: function(success, error, args) {
-        console.log('Client::startEngineWithScript', arguments);
         Electron.ipcRenderer.once('startEngineWithScriptResponse', (event, ...args) => {
-            console.log('Client::startEngineWithScript::Result', ...args);
             success(args);
         });
         Electron.ipcRenderer.send('startEngineWithScript', args);
     },
-    setAllChannelsListener: function(success, error, args) {
-        console.log('Client::setAllChannelsListener', arguments);
-        Electron.ipcRenderer.once('setAllChannelsListenerResponse', (event, ...args) => {
-            console.log('Client::setAllChannelsListener::Result', ...args);
-            success(args);
-        });
-        Electron.ipcRenderer.send('setAllChannelsListener', args);
-    }
+    setAllChannelsListener: function(success, error, args) {}
 }
 cordova.commandProxy.add('NodeJS', exports);
