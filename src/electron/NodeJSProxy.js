75f9939db33082dda8ba84018e5ceb6214d7b5cc
Dbl O Sevn (Doug) <doug@geauxpreaux.tech>
2020-09-30 02:45:53 -0500

    Quit using invoke it doesnt exist for in the current version  of electron.

--- src/electron/NodeJSProxy.js
+++ src/electron/NodeJSProxy.js
@@ -1,27 +1,30 @@
 module.exports = exports = {
     sendMessageToNode: function(success, error, args) {
-        console.log('Client::startEngine::Result', result);
+        console.log('Client::sendMessageToNode', args);
         Electron.ipcRenderer.send('sendMessageToNode', args);
     },
     startEngine: function(success, error, args) {
         console.log('Client::startEngine', args);
-        Electron.ipcRenderer.invoke('startEngine').then((result) => {
-            console.log('startEngine::Result', result);
-            success(result);
+        Electron.ipcRenderer.send('startEngine');
+        Electron.ipcRenderer.once('startEngineResponse', (event, ...args) => {
+            console.log('Client::startEngine::Result', ...args);
+            success(args);
         });
     },
     startEngineWithScript: function(success, error, args) {
         console.log('Client::startEngineWithScript', arguments);
-        Electron.ipcRenderer.invoke('startEngineWithScript', args).then((result) => {
-            console.log('Client::startEngineWithScript::Result', result);
-            success(result);
+        Electron.ipcRenderer.send('startEngineWithScript', args);
+        Electron.ipcRenderer.once('startEngineWithScriptResponse', (event, ...args) => {
+            console.log('Client::startEngineWithScript::Result', ...args);
+            success(args);
         });
     },
     setAllChannelsListener: function(success, error, args) {
         console.log('Client::setAllChannelsListener', arguments);
-        Electron.ipcRenderer.invoke('setAllChannelsListener', args).then((result) => {
-            console.log('Client::setAllChannelsListener::Result', result);
-            success(result);
+        Electron.ipcRenderer.send('setAllChannelsListener', args);
+        Electron.ipcRenderer.once('setAllChannelsListenerResponse', (event, ...args) => {
+            console.log('Client::setAllChannelsListener::Result', ...args);
+            success(args);
         });
     }
 }
